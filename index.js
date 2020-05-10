b697f098dce625b2ae79e15244032424c7222823
danby14 <51226470+danby14@users.noreply.github.com>
2020-05-10 03:24:07 -0500

    Added skipped and adjustable countdown timer features.

--- index.js
+++ index.js
@@ -12,12 +12,15 @@ let taken = [];
 let topicsIndex;
 let correctCounter = 0;
 
+const changeTimeButtons = document.querySelectorAll('.pick-time button');
 const categories = document.querySelectorAll('.profession-selector li');
 const alreadyGot = document.querySelector('.correct-guesses ul');
 const initials = document.querySelector('.initials');
 const guess = document.querySelector('input');
 const lastGuess = document.querySelector('.last-wrong-guess');
-const button = document.querySelector('button');
+const button = document.querySelector('.guesses button');
+const totalSkipped = document.querySelector('.skipped .number');
+const countdown = document.querySelector('.pick-time .number');
 const totalCorrect = document.querySelector('.correct .number');
 const hint1 = document.querySelector('.hint');
 const hint2 = document.querySelectorAll('.hint')[1];
@@ -32,10 +35,59 @@ function getRandom(min, max) {
 
 let selected;
 let solution;
+let topic;
+
+let time;
+let initialTime = 90;
+countdown.textContent = initialTime;
+let timerId;
+function timer() {
+  if (time <= 0) {
+    clearInterval(timerId);
+    enableTimerChange();
+    initials.textContent = 'Game Over';
+    solution = undefined;
+  }
+  countdown.textContent = time;
+  time -= 1;
+  if (time === -1) {
+    console.log('cat');
+    countdown.textContent = initialTime;
+  }
+}
+
+const startTimer = () => {
+  timer();
+  timerId = setInterval(timer, 1000);
+};
+
+// startTimer();
+
+function subtractTime() {
+  initialTime -= 1;
+  countdown.textContent = initialTime;
+}
+function addTime() {
+  initialTime += 1;
+  countdown.textContent = initialTime;
+}
+
+function disableTimerChange() {
+  changeTimeButtons[0].disabled = true;
+  changeTimeButtons[1].disabled = true;
+}
+
+function enableTimerChange() {
+  changeTimeButtons[0].disabled = false;
+  changeTimeButtons[1].disabled = false;
+}
+
+changeTimeButtons[0].addEventListener('click', subtractTime);
+changeTimeButtons[1].addEventListener('click', addTime);
 
 function getNewInitials() {
-  let topic = topics[topicsIndex];
-  34;
+  lastGuess.textContent = '';
+  topic = topics[topicsIndex];
   let random = getRandom(0, topic.length - 1);
   if (topic.length < 1) {
     return (solution = undefined);
@@ -51,14 +103,15 @@ function getNewInitials() {
 categories.forEach((x, i) =>
   x.addEventListener('click', () => {
     // to reset correct counter on category change
-    // correctCounter = 0;
-    // totalCorrect.textContent = correctCounter;
+    correctCounter = 0;
+    totalCorrect.textContent = correctCounter;
 
-    // alreadyGot.textContent = '';
-    // taken = [];
+    alreadyGot.textContent = '';
+    totalSkipped.textContent = 0;
+    taken = [];
     if (selected) {
-      let canada = document.querySelector('.active');
-      canada.classList.toggle('active');
+      let active = document.querySelector('.active');
+      active.classList.toggle('active');
     }
     x.classList.toggle('active');
     selected = x.textContent;
@@ -68,6 +121,10 @@ categories.forEach((x, i) =>
     if (initials.textContent === '') {
       initials.textContent = 'Got Them All';
     }
+    clearInterval(timerId);
+    disableTimerChange();
+    time = initialTime;
+    startTimer();
   })
 );
 
@@ -78,7 +135,8 @@ guess.onkeydown = e => {
 };
 
 button.addEventListener('click', () => {
-  if (guess.value.length < 1 || solution.name === undefined) return;
+  if (time < 0) return;
+  if (guess.value.length < 1 || solution === undefined) return;
   if (guess.value.toLowerCase() === solution.name.toLowerCase()) {
     console.log('correct');
     correctCounter += 1;
@@ -94,22 +152,22 @@ button.addEventListener('click', () => {
     getNewInitials();
     if (solution === undefined) {
       initials.textContent = 'Got Them All';
+      clearInterval(timerId);
     } else {
       initials.textContent = getNewInitials();
     }
-
-    lastGuess.textContent = '';
+    resetHints();
   } else {
     console.log('wrong');
     lastGuess.textContent = guess.value;
   }
-  resetHints();
   guess.value = '';
 });
 
 function resetHints() {
   hint1.textContent = 'hint 1';
   hint2.textContent = 'hint 2';
+  skip.textContent = 'skip';
 }
 
 hint1.addEventListener('click', () => {
@@ -117,4 +175,18 @@ hint1.addEventListener('click', () => {
 });
 hint2.addEventListener('click', () => {
   if (solution) hint2.textContent = solution.hint2;
+});
+
+let skipped = 0;
+skip.addEventListener('click', () => {
+  if (solution) {
+    if (topic.length < 2) return (skip.textContent = 'no more skips');
+    // lastGuess.textContent = '';
+    resetHints();
+    guess.value = '';
+    getNewInitials();
+    skipped += 1;
+    totalSkipped.textContent = skipped;
+    initials.textContent = getNewInitials();
+  }
 });
