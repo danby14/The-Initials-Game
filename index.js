3c076ccf6c5380159eb15a55dae4110f0bf8f1f2
danby14 <51226470+danby14@users.noreply.github.com>
2020-05-11 04:02:35 -0500

    some refactoring and minor adjustments.

--- index.js
+++ index.js
@@ -7,51 +7,48 @@ import Nhl from './people/nhl.js';
 import Mlb from './people/mlb.js';
 import Other from './people/other.js';
 
-const topics = [Actors, Music, MiscSports, Nba, Mlb, Nfl, Nhl, Other];
-let taken = [];
-let topicsIndex;
-let correctCounter = 0;
-
-const changeTimeButtons = document.querySelectorAll('.pick-time button');
 const categories = document.querySelectorAll('.profession-selector li');
+const outOf = document.getElementById('outOf');
 const alreadyGot = document.querySelector('.correct-guesses ul');
 const initials = document.querySelector('.initials');
 const guess = document.querySelector('input');
 const lastGuess = document.querySelector('.last-wrong-guess');
 const button = document.querySelector('.guesses button');
+
 const totalSkipped = document.querySelector('.skipped .number');
 const countdown = document.querySelector('.pick-time .number');
+const changeTimeButtons = document.querySelectorAll('.pick-time button');
 const totalCorrect = document.querySelector('.correct .number');
+
 const hint1 = document.querySelector('.hint');
 const hint2 = document.querySelectorAll('.hint')[1];
 const hint3 = document.querySelectorAll('.hint')[2];
 const skip = document.querySelectorAll('.hint')[3];
 
-function getRandom(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-let selected;
-let solution;
-let topic;
-
-let time;
+let correctCounter = 0;
 let initialTime = 90;
-countdown.textContent = initialTime;
+let selected;
+let skipped = 0;
+let solution;
+let taken = [];
+let time;
 let timerId;
+let topic;
+const topics = [Actors, Music, MiscSports, Nba, Mlb, Nfl, Nhl, Other];
+let topicsIndex;
+
+countdown.textContent = initialTime;
+
 function timer() {
   if (time <= 0) {
     clearInterval(timerId);
-    enableTimerChange();
+    disableTimerChange(false);
     initials.textContent = 'Game Over';
     solution = undefined;
   }
   countdown.textContent = time;
   time -= 1;
   if (time === -1) {
-    console.log('cat');
     countdown.textContent = initialTime;
   }
 }
@@ -61,33 +58,29 @@ const startTimer = () => {
   timerId = setInterval(timer, 1000);
 };
 
-// startTimer();
-
-function subtractTime() {
-  initialTime -= 1;
-  countdown.textContent = initialTime;
-}
-function addTime() {
-  initialTime += 1;
+function changeTime(a) {
+  console.log(a);
+  if (a === 'sub') initialTime -= 10;
+  if (a === 'add') initialTime += 10;
   countdown.textContent = initialTime;
 }
 
-function disableTimerChange() {
-  changeTimeButtons[0].disabled = true;
-  changeTimeButtons[1].disabled = true;
+changeTimeButtons[0].addEventListener('click', () => changeTime('sub'));
+changeTimeButtons[1].addEventListener('click', () => changeTime('add'));
+
+function disableTimerChange(bool) {
+  changeTimeButtons[0].disabled = bool;
+  changeTimeButtons[1].disabled = bool;
 }
 
-function enableTimerChange() {
-  changeTimeButtons[0].disabled = false;
-  changeTimeButtons[1].disabled = false;
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-changeTimeButtons[0].addEventListener('click', subtractTime);
-changeTimeButtons[1].addEventListener('click', addTime);
 
 function getNewInitials() {
   lastGuess.textContent = '';
-  topic = topics[topicsIndex];
   let random = getRandom(0, topic.length - 1);
   if (topic.length < 1) {
     return (solution = undefined);
@@ -100,33 +93,41 @@ function getNewInitials() {
   return solution.initials;
 }
 
-categories.forEach((x, i) =>
-  x.addEventListener('click', () => {
-    // to reset correct counter on category change
-    correctCounter = 0;
-    totalCorrect.textContent = correctCounter;
+function resetBoard() {
+  correctCounter = 0;
+  totalCorrect.textContent = correctCounter;
+  outOf.textContent = '';
+  alreadyGot.textContent = '';
+  totalSkipped.textContent = 0;
+  taken = [];
+  time = initialTime;
+  resetHints();
+  clearInterval(timerId);
+}
 
-    alreadyGot.textContent = '';
-    totalSkipped.textContent = 0;
-    taken = [];
-    if (selected) {
-      let active = document.querySelector('.active');
-      active.classList.toggle('active');
-    }
-    x.classList.toggle('active');
-    selected = x.textContent;
-    topicsIndex = i;
-    resetHints();
-    initials.textContent = getNewInitials();
-    if (initials.textContent === '') {
-      initials.textContent = 'Got Them All';
-    }
-    clearInterval(timerId);
-    disableTimerChange();
-    time = initialTime;
-    startTimer();
-  })
-);
+let topicOgLength;
+function onCategorySelect(x, i) {
+  resetBoard();
+
+  if (selected) {
+    let active = document.querySelector('.active');
+    active.classList.toggle('active');
+  }
+  x.classList.toggle('active');
+  selected = x.textContent;
+
+  topic = [...topics[i]];
+  topicOgLength = topics[i].length;
+  initials.textContent = getNewInitials();
+  if (initials.textContent === '') {
+    initials.textContent = 'Got Them All';
+  }
+
+  disableTimerChange(true);
+  startTimer();
+}
+
+categories.forEach((x, i) => x.addEventListener('click', () => onCategorySelect(x, i)));
 
 guess.onkeydown = e => {
   if (e.keyCode === 13) {
@@ -142,14 +143,14 @@ button.addEventListener('click', () => {
     correctCounter += 1;
     totalCorrect.textContent = correctCounter;
 
+    taken.push(solution.name);
+    outOf.textContent = `(${taken.length}/${topicOgLength})`;
     let li = document.createElement('li');
     li.appendChild(document.createTextNode(solution.name));
     alreadyGot.insertBefore(li, alreadyGot.childNodes[0]);
-    // alreadyGot.appendChild(li);
-
-    taken.push(solution.name);
 
     getNewInitials();
+
     if (solution === undefined) {
       initials.textContent = 'Got Them All';
       clearInterval(timerId);
@@ -177,11 +178,9 @@ hint2.addEventListener('click', () => {
   if (solution) hint2.textContent = solution.hint2;
 });
 
-let skipped = 0;
 skip.addEventListener('click', () => {
   if (solution) {
     if (topic.length < 2) return (skip.textContent = 'no more skips');
-    // lastGuess.textContent = '';
     resetHints();
     guess.value = '';
     getNewInitials();
